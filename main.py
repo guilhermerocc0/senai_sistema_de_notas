b3adb7a0ac94e604c080bc003e353737e0187fe9
GUILHERME RIBEIRO ROCCO <47199630808@info.local>
2026-03-16 20:14:22 -0300

    Adicionando codigo main

---
+++ main.py
@@ -0,0 +1,12 @@
+import processamento
+
+# Estrutura pedida: Lista de Tuplas
+alunos = [
+    ("Ana", [8, 7, 9]),
+    ("Carlos", [6, 5, 7]),
+    ("Maria", [10, 9, 8]),
+    ("Joao", [6, 6, 5]),
+    ("Lucas", [])
+]
+
+processamento.processar_alunos(alunos)
